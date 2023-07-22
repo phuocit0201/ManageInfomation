2efeaac0e86910f15cfae4d3a5a1235454b6670a
vietphuocit <vietphuocit@gmail.com>
2023-07-22 20:41:23 +0700

    fix

--- views/client/layout.php
+++ views/client/layout.php
@@ -61,8 +61,5 @@
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
-<<<<<<< HEAD
 
-=======
->>>>>>> 0b0aea402f15ef00fa88d724b71487992591ae5b
 </html>
