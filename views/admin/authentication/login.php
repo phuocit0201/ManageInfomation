c320a3332ea10153e9f2c3975e607d8cd0d12faa
Nghia <19142201@student.hcmute.edu.vn>
2023-08-06 22:56:37 +0700

    update login

--- views/admin/authentication/login.php
+++ views/admin/authentication/login.php
@@ -2,72 +2,127 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="author" content="MatrrDigital">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="robots" content="noindex, nofollow">
+  <title>Login | Quản lý Hồ sơ đến</title>
+  <link rel="shortcut icon" href="public/asset/logo-hcmute.png" type="image/x-icon" />
   <base href="<?= base ?>">
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="public/plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="public/dist/css/adminlte.min.css">
+  <!-- ================== BEGIN PAGE LEVEL CSS START ================== -->
+  <link rel="stylesheet" href="public/asset/owl.carousel.min.css" />
+  <!-- ================== BEGIN PAGE LEVEL END ================== -->
+  <!-- ================== Plugins CSS  ================== -->
+  <link rel="stylesheet" href="public/asset/owl.carousel.min.css" />
+  <!-- ================== Plugins CSS end ================== -->
+  <!-- ================== BEGIN APP CSS  ================== -->
+  <link rel="stylesheet" href="public/asset/bootstrap.min.css" />
+  <link rel="stylesheet" href="public/asset/styles.min.css">
+  <!-- ================== END APP CSS ================== -->
 </head>
 
-<body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href=""><b>ADMINISTRATOR</b></a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="card">
-      <div class="card-body login-card-body">
-        <p class="login-box-msg">AUTHENTICATION</p>
-        <?php
-        if (isset($_SESSION['message'])) {
-          echo "<p class='text-danger'>" . $_SESSION['message'] . "</p>";
-          unset($_SESSION['message']);
-        }
-        ?>
-        <form action="<?=route('login')?>" method="post">
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" name="username" placeholder="Username">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
+<body>
+  <!-- Begin Page -->
+  <div class="auth-pages">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-11 col-xl-10">
+          <div class="row justify-content-center">
+            <div class="col-md-6 pe-md-0">
+              <div class="auth-page-sidebar overflow-hidden">
+                <div class="overlay"> 
+                <img src="public/asset/hinh3.png" height="800" alt="Lettstart Admin">
+                </div>
+                <div class="auth-user-testimonial">
+                  <div class="owl-carousel">
+                  <div class="item" style="color: red;">
+                    <h3 class="text-white mb-1" style="color: red !important;">Welcome Admin!</h3>
+                    <h5 class="text-white mb-3" style="color: #014EC4 !important;">"Chào mừng Bạn đến với Trang quản lý Văn bản đến của Văn phòng Đảng ủy
+                      Trường Đại học Sư phạm Kỹ thuật TP. Hồ Chí Minh"</h5>
+                    <p style="color: red;">- Văn phòng Đảng ủy -</p>
+                  </div>
+                  <div class="item" style="color: red;">
+                    <h3 class="text-white mb-1" style="color: red !important;">Bye Bye User!</h3>
+                    <h5 class="text-white mb-3" style="color: #014EC4 !important;">"Chúng tôi rất lấy làm tiếc khi bạn không thể truy cập vào Trang quản lý này
+                      . Mọi thắc mắc bạn có thể liên hệ Văn phòng Đảng ủy để được giải quyết trực tiếp nhé ! "</h5>
+                    <p style="color: red;">- Văn phòng Đảng ủy -</p>
+                  </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" class="form-control" name="password" placeholder="Password">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <!-- /.col -->
-            <div class="col-12 text-center">
-              <button type="submit" class="btn btn-primary btn-block">Login</button>
-            </div>
-            <!-- /.col -->
-          </div>
-        </form>
+            <div class="col-md-6 ps-md-0">
+              <div class="card mb-0 p-2 p-md-3 border-bl-radius-0 border-tl-radius-0">
+                <div class="card-body">
+                  <div class="clearfix">
+                    <img src="public/asset/logo-hcmute.png" height="300" alt="Lettstart Admin">
+                  </div>
+                  <div style="display: flex; justify-content: center;">
+                    <h5 class="mt-4 fw-semibold" style="color: red !important;">Welcome Admin!</h5>
+                  </div>
+                  <form action="<?=route('login')?>" method="post" id="loginForm" novalidate>
+                    <div class="form-group floating-label">
+                      <input type="email" class="form-control form-control-lg" name="username" id="email" />
+                      <label for="email">Username</label>
+                    </div>
+                    <div class="form-group floating-label">
+                      <input type="password" class="form-control form-control-lg" name="password" id="password" />
+                      <label for="password">Password</label>
+                    </div>
 
-        <!-- /.login-card-body -->
+                    <div class="form-group text-center d-grid">
+                      <button class="btn btn-primary" data-effect="wave" type="submit"> Log In
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <!-- end row -->
     </div>
-    <!-- /.login-box -->
+    <!-- end container -->
+  </div>
+  <!-- Page End -->
+   <!-- ================== BEGIN BASE JS ================== -->
+   <script src="public/asset/vendor.min.js"></script>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+  <!-- ================== END BASE JS ================== -->
 
-    <!-- jQuery -->
-    <script src="public/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="public/dist/js/adminlte.min.js"></script>
+  <!-- ================== BEGIN PAGE LEVEL JS ================== -->
+  <script src="public/asset/colors.js"></script>
+  <script src="public/asset/owl.carousel.min.js"></script>
+  <script src="public/asset/jquery.validate.min.js"></script>
+  <script src="public/asset/additional-methods.min.js"></script>
+  <!-- ================== END PAGE LEVEL JS ================== -->
+  <!-- ================== BEGIN PAGE JS ================== -->
+  <script src="public/asset/app.js"></script>
+  <!-- ================== END PAGE JS ================== -->
+  <script>
+    $(".auth-user-testimonial .owl-carousel").owlCarousel({
+      loop: true,
+      margin: 0,
+      nav: false,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+    });
+  
+  </script>
 </body>
 
 </html>
